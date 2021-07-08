120608909f1af0750a061fd3c56627fe5300357c
GeorgePetts <gpetts@cmittorrance.com>
2021-07-07 18:04:58 -0700

    dr

---
+++ DigitalRoot/DigitalRoot/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Linq;
+
+namespace DigitalRoot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Number.DigitalRoot(456));
+        }
+    }
+    public class Number
+    {
+        public static int DigitalRoot(long n)
+        {
+            int dr = n.ToString().ToCharArray().Select(c => Convert.ToInt32(c.ToString())).Sum();
+            if (dr < 10)
+                return dr;
+            else
+                return DigitalRoot(dr);
+        }
+    }
+}
