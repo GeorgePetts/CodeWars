e59e42f198d039dd26a46e6fbdc3a43784b64a0e
GeorgePetts <gpetts@cmittorrance.com>
2021-07-07 17:31:41 -0700

    bit count

---
+++ BitCounting/BitCounting/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace BitCounting
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Kata.CountBits(256));
+        }
+    }
+    public class Kata
+    {
+        public static int CountBits(int n)
+        {
+            UInt32 un = (uint)n;
+            UInt32 numbits = 0;
+            while (un > 0)
+            {
+                numbits += (un & 1);
+                un = un >> 1;
+            }
+            return (int)numbits;
+        }
+    }
+}
