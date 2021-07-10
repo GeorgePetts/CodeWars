291a2dc09cab13ed5ed6f2f19afacbc4392216b9
GeorgePetts <gpetts@cmittorrance.com>
2021-07-09 23:36:08 -0700

    discard

---
+++ ProdFib/ProdFib/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace ProdFib
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+    public class ProdFib
+    {
+        public static ulong[] productFib(ulong prod)
+        {
+            return null;
+        }
+    }
+}
