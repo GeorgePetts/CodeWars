6c069d0e97b7ca024667064a2eda470371c30af8
GeorgePetts <gpetts@cmittorrance.com>
2021-07-10 10:58:00 -0700

    commit

--- ProdFib/ProdFib/Program.cs
+++ ProdFib/ProdFib/Program.cs
@@ -6,14 +6,40 @@ namespace ProdFib
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine(ProdFib.productFib(447577));
         }
     }
     public class ProdFib
     {
         public static ulong[] productFib(ulong prod)
         {
-            return null;
+            ulong NMinus2 = 0;
+            ulong NMinus1 = 1;
+            ulong MaxNMinus2 = Convert.ToUInt64(Math.Sqrt(prod));
+            ulong prodSum;
+            ulong prodTest = 0;
+
+            if (NMinus1 * NMinus2 == prod)
+            {
+                ulong[] result1= { NMinus2, NMinus1, 1};
+                return result1;
+            }
+            while (NMinus2 < MaxNMinus2 && prodTest < prod)
+            {
+                prodSum = NMinus2 + NMinus1;
+                NMinus2 = NMinus1;
+                NMinus1 = prodSum;
+
+
+                prodTest = NMinus1 * NMinus2;
+                if (prodTest == prod)
+                {
+                    ulong[] result2 = { NMinus2, NMinus1, 1 };
+                     return result2;
+                }
+            }
+            ulong[] result = { NMinus2, NMinus1, 0 };
+            return result;
         }
     }
 }
